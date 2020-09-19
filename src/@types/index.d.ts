4cdad2413503e31931b939b23125e2e2742ba785
Kaique Araújo <69827216+gottium-kaique@users.noreply.github.com>
2020-09-18 21:00:03 -0300

    Arquivos do Projeto

---
+++ src/@types/index.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background?: string
+    color?: string
+    colorInDarkSection?: string
+    primary?: string
+  }
+}
